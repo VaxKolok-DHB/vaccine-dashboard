a37deea4a0291f07cf336ef591e7e78c0a0848d7
NURSAREEN2627 <120258174+NURSAREEN2627@users.noreply.github.com>
2026-05-06 15:02:12 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const axios = require("axios");
+
+const app = express();
+app.use(express.json());
+
+const TOKEN = "DIK8oggf4sTTqeGzpc+PnWOX/4g+rGQOt4x/E7+b7uxOT0nSQcpU/O8to6IZgIOAzRpfGzesWr5Gh+P0EAH6gT KJ+lhqyOIVGOgS+o9cY3S3h6+l0vY1sMQ0hmZDKOaNu6zkfaYL+4unZLnjWLJBdgdB04t89/1O/w1cDnyilFU=node server.js";
+
+app.post("/send", async (req, res) => {
+
+  const { name, userId } = req.body;
+
+  try{
+    await axios.post(
+      "https://api.line.me/v2/bot/message/push",
+      {
+        to: userId,
+        messages: [
+          {
+            type: "text",
+            text: `👶 ${name} ฉีดวัคซีนแล้ว\n\n⏰ น้องมีอาการอย่างไรบ้าง?`
+          }
+        ]
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${TOKEN}`
+        }
+      }
+    );
+
+    res.send("sent");
+
+  }catch(err){
+    console.log(err.response?.data || err.message);
+    res.send("error");
+  }
+
+});
+
+app.listen(3000, () => console.log("server running"));
